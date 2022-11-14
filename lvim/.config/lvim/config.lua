87843caf70a96f5daf4f66eae8a5c239e00024e2
Lex Beelen <lex@lexbeelen.nl>
2022-11-14 21:15:11 +0100

    Change terminal direction to float

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -18,7 +18,7 @@ lvim.builtin.which_key.mappings["t"] = {
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
-lvim.builtin.terminal.direction = "horizontal"
+lvim.builtin.terminal.direction = "float"
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 

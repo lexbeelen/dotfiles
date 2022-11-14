18952143c0cdd7257149e79b7c60256a487bd4df
Lex Beelen <lex@lexbeelen.nl>
2022-11-14 19:49:25 +0100

    Initial commit

---
+++ lvim/.config/lvim/config.lua
@@ -0,0 +1,104 @@
+lvim.log.level = "warn"
+lvim.format_on_save.enabled = true
+lvim.colorscheme = "tokyonight"
+lvim.transparent_window = true
+lvim.leader = "space"
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+}
+
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.terminal.active = true
+lvim.builtin.terminal.direction = "horizontal"
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "php",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enable = true
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    command = "prettier",
+    extra_args = { "--print-with", "100" },
+    filetypes = { "typescript", "typescriptreact" },
+  },
+  {
+    command = "phpcsfixer",
+    filetypes = { "php" }
+  },
+  {
+    command = "blade-formatter",
+    filetypes = { "php" }
+  }
+}
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  { command = "eslint", filetypes = { "typescript", "typescriptreact" } },
+  { command = "phpcs", filetypes = { "php" } },
+  { command = "phpstan", filetypes = { "php" } }
+}
+
+lvim.plugins = {
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  {
+    "jwalton512/vim-blade"
+  },
+}
+
+local lsp_manager = require("lvim.lsp.manager")
+lsp_manager.setup("intelephense")
+
+local dap = require("dap")
+local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
+dap.adapters.php = {
+  type = "executable",
+  command = "node",
+  args = { mason_path .. "packages/php-debug-adapter/extension/out/phpDebug.js" },
+}
+dap.configurations.php = {
+  {
+    name = "Listen for Xdebug",
+    type = "php",
+    request = "launch",
+    port = 9003,
+  },
+  {
+    name = "Debug currently open script",
+    type = "php",
+    request = "launch",
+    port = 9003,
+    cwd = "${fileDirname}",
+    program = "${file}",
+    runtimeExecutable = "php",
+  },
+}

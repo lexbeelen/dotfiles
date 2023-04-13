4462fb773b3d21a9a0611d5ac09cf77f470fc618
Lex Beelen <lex.beelen@iodigital.com>
2023-04-13 15:22:31 +0200

    Autocomplete for kubernetes

--- nvim/lua/me/plugins/lsp.lua
+++ nvim/lua/me/plugins/lsp.lua
@@ -1,12 +1,12 @@
 require("mason").setup()
-require("mason-lspconfig").setup {
-    ensure_installed = { "tsserver", "intelephense", "tailwindcss", "eslint"},
-    automatic_installation = true,
-}
+require("mason-lspconfig").setup({
+  ensure_installed = { "tsserver", "intelephense", "tailwindcss", "eslint" },
+  automatic_installation = true,
+})
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 -- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 -- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 -- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
@@ -16,36 +16,60 @@ local opts = { noremap=true, silent=true }
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
   -- Mappings.
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
   -- vim.keymap.set('n', '<leader>fm', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
-require('lspconfig')['tsserver'].setup{
-    on_attach = on_attach,
-    capabilities = capabilities,
-}
-
-require('lspconfig')['tailwindcss'].setup{
+require("lspconfig")["tsserver"].setup({
   on_attach = on_attach,
   capabilities = capabilities,
-}
+})
 
-require('lspconfig')['intelephense'].setup{
-    on_attach = on_attach,
-    capabilities = capabilities,
-}
+require("lspconfig")["tailwindcss"].setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
 
-require('lspconfig')['jsonls'].setup({
+require("lspconfig")["intelephense"].setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+require("lspconfig")["yamlls"].setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    yaml = {
+      schemas = {
+        kubernetes = "*.yaml",
+        ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+        ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+        ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+        ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+        ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+        ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+        ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+        ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+        ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+        ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+        ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+      },
+    },
+  },
+})
+
+require("lspconfig")["jsonls"].setup({
   on_attach = on_attach,
   capabilities = capabilities,
   settings = {
     json = {
-      schemas = require('schemastore').json.schemas(),
+      schemas = require("schemastore").json.schemas(),
     },
   },
 })

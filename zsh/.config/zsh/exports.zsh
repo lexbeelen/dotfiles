ef04efbde82a896a861c5495af6130930da21422
Lex Beelen <lex@lexbeelen.nl>
2023-06-07 19:17:51 +0200

    Export Path with full $HOME path

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -7,7 +7,7 @@ export TERMINAL='kitty'
 
 export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
-export PATH=".local/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 export PATH="/var/lib/snapd/snap/bin:$PATH"
 
 [[ -n $TMUX ]] && export TERM="xterm-256color"

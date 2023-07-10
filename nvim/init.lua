e1f566f61f2fd6608cc9a44a276d30f5a292742c
Lex Beelen <lex@lexbeelen.nl>
2023-07-10 22:49:12 +0200

    Enable TSServer

--- nvim/init.lua
+++ nvim/init.lua
@@ -312,7 +312,7 @@ require('nvim-treesitter.configs').setup {
   ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'php' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-  auto_install = false,
+  auto_install = true,
 
   highlight = { enable = true },
   indent = { enable = true },
@@ -432,7 +432,7 @@ local servers = {
   -- gopls = {},
   -- pyright = {},
   rust_analyzer = {},
-  -- tsserver = {},
+  tsserver = {},
 
   lua_ls = {
     Lua = {

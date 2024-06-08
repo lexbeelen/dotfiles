30be9821dcba8ce3460a6365490d99b87423cd67
Lex Beelen <lex@lexbeelen.nl>
2024-06-08 22:47:29 +0200

    Improving telescope

--- tvim/lua/plugins/telescope.lua
+++ tvim/lua/plugins/telescope.lua
@@ -1,28 +1,46 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.6",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local builtin = require("telescope.builtin")
+    event = "VimEnter",
+    branch = "0.1.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { -- If encountering errors, see telescope-fzf-native README for installation instructions
+        "nvim-telescope/telescope-fzf-native.nvim",
 
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-    end,
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
+        -- `build` is used to run some command when the plugin is installed/updated.
+        -- This is only run then, not every time Neovim starts up.
+        build = "make",
+
+        -- `cond` is a condition used to determine whether this plugin should be
+        -- installed and loaded.
+        cond = function()
+          return vim.fn.executable("make") == 1
+        end,
+      },
+      { "nvim-telescope/telescope-ui-select.nvim" },
+
+      -- Useful for getting pretty icons, but requires a Nerd Font.
+      { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+    },
     config = function()
+      -- [[ Configure Telescope ]]
       require("telescope").setup({
-        extension = {
-
+        extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
+            require("telescope.themes").get_dropdown(),
           },
         },
       })
-      require("telescope").load_extension("ui-select")
+
+      -- Enable Telescope extensions if they are installed
+      pcall(require("telescope").load_extension, "fzf")
+      pcall(require("telescope").load_extension, "ui-select")
+
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+      vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
     end,
   },
 }
